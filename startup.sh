f6197cf22f50dc639022eeceabc697ea2bc61d15
Srinidhi <shrinidhi.kaushik@gmail.com>
2016-03-29 00:23:09 +0530

    Fixed a typo.

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@
 # Runs on Guincorn with 32 workers.
 # The default port is 4080.
 
-# nohup gunicorn -w 4 -b 0.0.0.0:4080 server:app &
-/usr/bin/reds-server
+/usr/bin/redis-server
 /usr/bin/env python2.7 init_redis.py
 nohup gunicorn --access-logfile /tmp/flask-access.log --error-logfile /tmp/flask-error.log --bind 0.0.0.0:80 --workers 32 server:app &
